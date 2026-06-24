03a980c592d62bb8e3295cb5bdf4a6c4cd55f564
Marcos Bodi Puchalt <TU_EMAIL_DE_GITHUB>
2026-06-25 00:19:28 +0200

    Restore homepage project order

--- js/render-index.js
+++ js/render-index.js
@@ -479,13 +479,8 @@ function setupIndexVideoPoster(video, poster) {
 
 if (projectGrid && window.PORTFOLIO_PROJECTS) {
   projectGrid.innerHTML = "";
-  const indexProjects = [...window.PORTFOLIO_PROJECTS];
 
-  if (indexProjects.length > 1) {
-    [indexProjects[0], indexProjects[1]] = [indexProjects[1], indexProjects[0]];
-  }
-
-  indexProjects
+  window.PORTFOLIO_PROJECTS
     .filter((project) => !project.hidden)
     .forEach((project, index) => {
       const card = document.createElement("a");
